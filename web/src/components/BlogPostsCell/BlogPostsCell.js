fdacc42afce40089176424055417a4e78afc1e1d
Shenzhen YAO <junk.them@hotmail.com>
2021-02-27 12:36:01 -0600

    20210227a

---
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -0,0 +1,38 @@
+import { Link, routes } from '@redwoodjs/router'
+// import BlogPost from 'src/components/BlogPost'
+
+export const QUERY = gql`
+  query BlogPostsQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ posts }) => {
+  // return JSON.stringify(posts)
+  return posts.map((post) => (
+    <article key={post.id}>
+      <header>
+        <h2>
+          <Link to={routes.blogPost({id: post.id})}>{post.title}</Link>
+        </h2>
+      </header>
+      <p>{post.body}</p>
+      <div>Posted at: {post.createdAt}</div>
+    </article>
+  ))
+}
+
+// export const Success = ({ posts }) => {
+//   return posts.map((post) => <BlogPost key={post.id} post={post} />)
+// }
